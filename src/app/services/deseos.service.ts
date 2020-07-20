6779016a9feb030f2c5ba9748550da109f1305d4
Pablo Rivera <pabloriverar.sv@gmail.com>
2020-07-20 10:18:56 -0600

    actializacion de app despues de restauracion de sistema

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista []=[];
+
+  constructor() {
+
+    const lista1 = new Lista('Recolectar Piedras del Infinito');
+    const lista2 = new Lista('Heroes a desaparecer');
+
+    this.listas.push (lista1, lista2);
+
+   
+
+   }
+
+}
